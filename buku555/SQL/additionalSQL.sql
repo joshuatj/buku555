08e7389a6fc48dc655c27c54c789f87c3ac36262
ducnq135 <duc.nguyen135@gmail.com>
2014-03-28 11:19:36 +0800

    duc's commit

---
+++ buku555/SQL/additionalSQL.sql
@@ -0,0 +1,4 @@
+-- This file is for any additional sql queries
+-- no need to rerun the original one
+
+ALTER TABLE `buku555`.`bill_splitees` CHANGE COLUMN `amount_to_pay` `amount_to_pay` DOUBLE NULL DEFAULT NULL  ;
